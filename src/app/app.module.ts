5d93c7616635073530dcc86bf66be1c2691a8d7b
oMullerr <leindorfmuller@gmail.com>
2022-10-22 17:24:47 -0300

    add routers application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { CabecalhoComponent } from './components/cabecalho/cabecalho.component';
 import { RodapeComponent } from './components/rodape/rodape.component';
 import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { FormsModule } from '@angular/forms';
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
 
 @NgModule({
-  declarations: [AppComponent, CabecalhoComponent, RodapeComponent, CriarPensamentoComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [
+    AppComponent,
+    CabecalhoComponent,
+    RodapeComponent,
+    CriarPensamentoComponent,
+    ListarPensamentoComponent,
+  ],
+
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
+
   providers: [],
+
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.css']
+  styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
+  pensamento = {
+    id: '1',
+    conteudo: 'Penso, logo existo',
+    autoria: 'Descartes',
+    modelo: 'modelo1',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  public criaPensamento(): void {
+    alert('Pensamento criado com sucesso!');
   }
 
+  public cancela(): void {
+    alert('Cancelado!');
+  }
 }
