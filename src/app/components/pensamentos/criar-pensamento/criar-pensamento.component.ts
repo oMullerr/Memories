84ca9fb145802177ae0a08e5b2c1235b0b117b3e
oMullerr <leindorfmuller@gmail.com>
2022-10-29 23:19:25 -0300

    add reactive forms and error message

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { PensamentoService } from './../pensamento.service';
 import { Pensamento } from './../pensamento';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,24 +10,46 @@ import { Router } from '@angular/router';
   styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent implements OnInit {
-  pensamento: Pensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: 'modelo1',
-  };
 
-  constructor(private service: PensamentoService,
-    private router: Router) {}
+  formulario!: FormGroup;
 
-  ngOnInit(): void {}
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['',Validators.compose([Validators.required, Validators.pattern(/(.|\s)*\S(.|\s)*/)])],
+      autoria: ['', Validators.compose([Validators.required, Validators.minLength(3)])],
+      modelo: ['modelo1']
+    });
+
+  }
 
   public criaPensamento(): void {
-    this.service.criar(this.pensamento).subscribe(() => {
+    console.log(this.formulario.get('autoria')?.errors);
+    if (this.formulario.valid) {
+      this.service.criar(this.formulario.value).subscribe(() => {
       this.router.navigate(['/listarPensamento']);
     });
+    }
+    // if(this.formulario.status === 'INVALID'){
+    //   alert('Preencha todos os campos corretamente!');
+    // }
   }
 
   public cancelar(): void {
     this.router.navigate(['/listarPensamento']);
   }
+
+  public habilitarBotao(): string {
+    if(this.formulario.valid){
+      return 'botao'
+    }
+    else{
+      return 'botao__desabilitado'
+    }
+  }
+
 }
